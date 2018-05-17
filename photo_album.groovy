f8f10aa37803c2307696a9d9ee665e870aea996f
bmeek24 <37094328+bmeek24@users.noreply.github.com>
2018-05-16 22:06:47 -0500

    Update photo_album
    Moved console prompt string to a variable for readability and encapsulated printPhotos as a new method.

--- photo_album.groovy
+++ photo_album.groovy
@@ -5,24 +5,24 @@ import java.util.regex.*
 def input = null
 
 while(!input?.equalsIgnoreCase("Q")) {
-    //User inputs desired photo album or photo
-    input = System.console().readLine 'Please input photo-album or photo-id followed by a number (ex: photo-album 3) OR enter Q to quit: '
+    String consoleDialog = 'Please input photo-album or photo-id followed by a number (ex: photo-album 3) OR enter Q to quit: '
+    input = System.console().readLine consoleDialog
     if (verifyInput(input)) {
-        //Print id and title of each photo in the console
-        JsonSlurper jsonSlurper = new JsonSlurper()
-        jsonSlurper.parse(buildURLFromInput(input)).each {
-            println("[" + it.id + "] " + it.title)
-        }
+        printPhotos(input)
     } else {
-        //Loop back to the beginning or quit
         (!input?.equalsIgnoreCase("Q")) ? println("Input " + input + " invalid. Please try again.") :
                 println("Goodbye")
     }
 }
 
+protected void printPhotos(input){
+    JsonSlurper jsonSlurper = new JsonSlurper()
+    jsonSlurper.parse(buildURLFromInput(input)).each {
+        println("[" + it.id + "] " + it.title)
+    }
+}
 
 protected Boolean verifyInput(String input){
-    //Input must be "photo-album # or photo-id #"
     return input =~ /(photo-album |photo-id )\d+/
 }
 
