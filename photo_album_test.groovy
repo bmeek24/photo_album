4503004f829acc134b58a6fa9c0373f917032655
bmeek24 <37094328+bmeek24@users.noreply.github.com>
2018-05-16 22:13:49 -0500

    Overhauled photo_album_test.groovy
    Moved test cases to a test class to give better feedback. Removed redundant tests and added new tests.

--- photo_album_test.groovy
+++ photo_album_test.groovy
@@ -1,23 +1,54 @@
+import junit.framework.Test
+import junit.framework.TestCase
+import junit.textui.TestRunner
 import photo_album
-import java.net.*
 
-final def classUnderTest = new photo_album()
 
-/**
- * Test verifyInput
- */
-assert classUnderTest.verifyInput("photo-album 3")
-assert classUnderTest.verifyInput("photo-id 3")
-assert !classUnderTest.verifyInput("phobo-id 3")
-assert !classUnderTest.verifyInput("dakjDBSJA")
-assert !classUnderTest.verifyInput("photo3")
+public class photo_album_tests extends TestCase {
 
-/**
- * Test buildURLFromInput
- */
-assert classUnderTest.buildURLFromInput("photo-album 3").sameFile(new URL("https://jsonplaceholder.typicode.com/photos?albumId=3"))
-assert classUnderTest.buildURLFromInput("photo-id 3").sameFile(new URL("https://jsonplaceholder.typicode.com/photos?id=3"))
+    final def photoAlbum = new photo_album()
 
-/**
- * TODO: Add unit tests for main once I figure out how to do stubbing in Groovy
- */
+    public void testShouldReturnFalseOnNull(){
+        assert !photoAlbum.verifyInput(null)
+    }
+
+    public void testShouldReturnFalseOnEmptyString(){
+        assert !photoAlbum.verifyInput("")
+    }
+
+    public void testShouldReturnTrueOnProperPhotoAlbumInput(){
+        assert photoAlbum.verifyInput("photo-album 3")
+    }
+
+    public void testShouldReturnTrueOnProperPhotoIdInput(){
+        assert photoAlbum.verifyInput("photo-id 3")
+    }
+
+    public void testShouldReturnFalseOnInvalidUrl(){
+        String input = ""
+        String expectedUrl = "https://jsonplaceholder.typicode.com/photos?albumId=3"
+        assert !photoAlbum.buildURLFromInput(input).sameFile(new URL(expectedUrl))
+    }
+
+    public void testShouldReturnTrueWhenBuildingUrlFromCommandStyle1(){
+        String input = "photo-album 3"
+        String expectedUrl = "https://jsonplaceholder.typicode.com/photos?albumId=3"
+        assert photoAlbum.buildURLFromInput(input).sameFile(new URL(expectedUrl))
+    }
+
+    public void testShouldReturnTrueWhenBuildingUrlFromCommandStyle2(){
+        String input = "photo-id 3"
+        String expectedUrl = "https://jsonplaceholder.typicode.com/photos?id=3"
+        assert photoAlbum.buildURLFromInput(input).sameFile(new URL(expectedUrl))
+    }
+}
+
+public class RunTests{
+    static Test suite() {
+        def allTests = new GroovyTestSuite()
+        allTests.addTestSuite(photo_album_tests.class)
+        return allTests
+    }
+}
+
+TestRunner.run(RunTests.suite())
